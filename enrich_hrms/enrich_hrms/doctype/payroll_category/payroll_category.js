5ec63c447d29a86d3e67b83c0b81f6e480396732
JignasaChavda <jignasha@sanskartechnolab.com>
2025-03-06 18:50:17 +0530

    Added payroll category

---
+++ enrich_hrms/enrich_hrms/doctype/payroll_category/payroll_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, jignasha@sanskartechnolab.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Payroll Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
